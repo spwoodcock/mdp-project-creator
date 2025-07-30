1f18b7b40aa375033698a161f4ceaf7925e322e2
spwoodcock <sam.woodcock@protonmail.com>
2025-07-30 16:06:46 +0100

    fix: update import script to call correct endpoints

--- import.py
+++ import.py
@@ -2,43 +2,49 @@
 
 import httpx
 
-# TODO batch requests if we need to do this for all of them + a pause between requests
 API_LIMIT = 1
+ORGANISATION_ID = 1
 SOURCE_URL = f"https://interativobe-mapadasperiferias.cidades.gov.br/api/favela-comunidade-urbana-2022/?format=json&limit={API_LIMIT}"
-DEST_URL = "https://api.mdp.fmtm.hotosm.org/projects"
+CREATE_PROJECT_URL = "https://api.mdp.fmtm.hotosm.org/projects/stub"
+EDIT_PROJECT_URL = f"https://api.mdp.fmtm.hotosm.org/projects?org_id={ORGANISATION_ID}"
 HEADERS = {"X-API-Key": "Bearer YOUR_TOKEN"}
 
-def transform_record(item: dict) -> dict:
-    """Transform source record to match your model fields."""
+def create_project_data(mdp_project: dict) -> dict:
+    """Transform MDP data into FieldTM project stub data."""
     return {
-        "id": item["id"],
-        "name": item["nm_fcu"],
+        "name": mdp_project["nm_fcu"],
         "outline": {
             "type": "Feature",
             "geometry": {
                 "type": "Polygon",
                 "coordinates": [[
-                    [item["bbox"][0], item["bbox"][1]],
-                    [item["bbox"][0], item["bbox"][3]],
-                    [item["bbox"][2], item["bbox"][3]],
-                    [item["bbox"][2], item["bbox"][1]],
-                    [item["bbox"][0], item["bbox"][1]],
+                    [mdp_project["bbox"][0], mdp_project["bbox"][1]],
+                    [mdp_project["bbox"][0], mdp_project["bbox"][3]],
+                    [mdp_project["bbox"][2], mdp_project["bbox"][3]],
+                    [mdp_project["bbox"][2], mdp_project["bbox"][1]],
+                    [mdp_project["bbox"][0], mdp_project["bbox"][1]],
                 ]]
             },
+            "id": mdp_project["id"],
             "properties": {
-                "cd_fcu": item["cd_fcu"],
-                "total_domicilios": item.get("total_domicilios")
+                "cd_fcu": mdp_project["cd_fcu"],
+                "total_domicilios": mdp_project.get("total_domicilios")
             }
         },
-        "short_description": item["nm_mun"],
-        "description": f"{item['nm_fcu']} in {item['nm_mun']} ({item['sigla_uf']})",
-        "slug": item["cd_fcu"].lower(),
-        "location_str": item["nm_mun"],
+        "short_description": mdp_project["nm_mun"],
+        "description": f"{mdp_project['nm_fcu']} in {mdp_project['nm_mun']} ({mdp_project['sigla_uf']})",
+        "status": "PUBLISHED",
+        "hashtags": ["mdp", "mapa-das-periferias"],
         "task_split_type": "manual",
-        "status": "published",
         "visibility": "private",
     }
-    # TODO add all required fields, including project manager
+
+def update_project_data(mdp_project: dict) -> dict:
+    """Transform MDP data into FieldTM project full data."""
+    return {
+        "task_split_type": "manual",
+        "visibility": "private",
+    }
 
 def main():
     # NOTE if we are doing this on a batch, we could optimise significantly
@@ -50,10 +56,29 @@ def main():
             r.raise_for_status()
             data = r.json()
             for item in data["results"]:
-                payload = transform_record(item)
-                post_resp = client.post(DEST_URL, json=payload, headers=HEADERS)
-                if post_resp.status_code != 201:
-                    print(f"Failed to insert {item['id']}: {post_resp.text}")
+                payload = create_project_data(item)
+
+                # 1. Basic project details
+                new_project = client.post(CREATE_PROJECT_URL, json=payload, headers=HEADERS)
+                if new_project.status_code > 204:
+                    print(f"Failed to insert {item['id']}: {new_project.text}")
+                    # Skip edit project
+                    continue
+                new_project_id = new_project.json().get("id")
+
+                # 2. Add remaining project details
+                updated_project = client.patch(f"{EDIT_PROJECT_URL}?project_id={new_project_id}", json=payload, headers=HEADERS)
+                if updated_project.status_code > 204:
+                    print(f"Failed to insert {item['id']}: {updated_project.text}")
+
+                # 3. TODO Upload survey
+                # 4. TODO Upload data
+                # 5. TODO Upload split boundaries
+                # 6. TODO Trigger project finalisation
+
+            # TODO when we run this for real, remove the break to interate
+            break
+            # TODO send these in asyncio.gather batches, with a sleep interval between
             url = data.get("next")
 
 if __name__ == "__main__":
